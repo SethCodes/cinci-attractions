9c087449271585b85172876034272f2ee4708ca5
Guillermo Cabral <guille.cabral87@gmail.com>
2021-09-02 13:35:33 -0400

    grid

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import './About.css';
 import Cont1_desk from '../media/cont1_desk.jpg';
 import Cont1_mobile from '../media/cont1_mobile.jpg';
 import Image1 from '../media/image1.jpg'
@@ -9,6 +9,13 @@ import TeamGEC from '../media/teamGEC.jpg'
 import LinkedIn from '../media/linkedIn.png'
 import Twitter from '../media/twitter.png'
 import Github from '../media/github.png'
+import ToPick1 from '../media/toPick1.jpg'
+import ToPick2 from '../media/toPick2.jpg'
+import ToPick3 from '../media/toPick3.jpg'
+import ToPick4 from '../media/toPick4.jpg'
+import ToPick5 from '../media/toPick5.jpg'
+import ToPick6 from '../media/toPick6.jpg'
+import ToPick7 from '../media/toPick7.jpg'
 
 
 const About = () => {
@@ -45,28 +52,28 @@ const About = () => {
                     <p id="p3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus quasi, vitae vero neque quis facere omnis, quibusdam est, delectus provident amet!</p>
                 </div>
                 <div id="team" className= "row">
-                    <div className="col center" id="teaMember">
+                    <div className="col-md-3 col-sm-12  center" id="teaMember">
                         <img src={TeamGEC} alt="GEC" id="teamPic"/>
                         <h5>Seth Stephens</h5>
                         <a href="#"><img src={LinkedIn} alt="linkedIn" id="teamSocial"/></a>
                         <a href="#"><img src={Twitter} alt="twitter" id="teamSocial"/></a>
                         <a href="#"><img src={Github} alt="github" id="teamSocial"/></a>
                     </div>
-                    <div className="col center" id="teaMember">
+                    <div className="col-md-3 col-sm-12  center" id="teaMember">
                         <img src={TeamGEC} alt="GEC" id="teamPic"/>
                         <h5>Guillermo Cabral</h5>
                         <a href="#"><img src={LinkedIn} alt="linkedIn" id="teamSocial"/></a>
                         <a href="#"><img src={Twitter} alt="twitter" id="teamSocial"/></a>
                         <a href="#"><img src={Github} alt="github" id="teamSocial"/></a>
                     </div>
-                    <div className="col center" id="teaMember">
+                    <div className="col-md-3 col-sm-12  center" id="teaMember">
                         <img src={TeamGEC} alt="GEC" id="teamPic"/>
                         <h5>Todd Cunningham</h5>
                         <a href="#"><img src={LinkedIn} alt="linkedIn" id="teamSocial"/></a>
                         <a href="#"><img src={Twitter} alt="twitter" id="teamSocial"/></a>
                         <a href="#"><img src={Github} alt="github" id="teamSocial"/></a>
                     </div>
-                    <div className="col center" id="teaMember">
+                    <div className="col-md-3 col-sm-12  center" id="teaMember">
                         <img src={TeamGEC} alt="GEC" id="teamPic"/>
                         <h5>Kushal Patel</h5>
                         <a href="#"><img src={LinkedIn} alt="linkedIn" id="teamSocial"/></a>
@@ -83,12 +90,34 @@ const About = () => {
                 </div>
             </div>
             <div className="container-4" >
-                <div className="col" id="content4">
+                <div className="" id="content4">
                     <h4 className="center" id="cont4h4">Our Top Picks</h4>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus quasi, vitae vero neque quis facere omnis, quibusdam est, delectus provident amet!</p>
                 </div>
-                <div>
-                    
+                <div className= "container">
+                    <div class="gallery">
+                        <div class="gallery__item gallery__item--1">
+                            <img src={ToPick1} class="gallery__img" alt="Image 1"/>
+                        </div>
+                        <div class="gallery__item gallery__item--2">
+                            <img src={ToPick2} class="gallery__img" alt="Image 2"/>
+                        </div>
+                        <div class="gallery__item gallery__item--3">
+                            <img src={ToPick3} class="gallery__img" alt="Image 3"/>
+                        </div>
+                        <div class="gallery__item gallery__item--4">
+                            <img src={ToPick4} class="gallery__img" alt="Image 4"/>
+                        </div>
+                        <div class="gallery__item gallery__item--5">
+                            <img src={ToPick5} class="gallery__img" alt="Image 5"/>
+                        </div>
+                        <div class="gallery__item gallery__item--6">
+                            <img src={ToPick6} class="gallery__img" alt="Image 6"/>
+                        </div>
+                        <div class="gallery__item gallery__item--7">
+                            <img src={ToPick7} class="gallery__img" alt="Image 7"/>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="container-5" >
