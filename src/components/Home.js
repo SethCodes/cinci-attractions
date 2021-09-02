ad1f922be5852ca718d220b2632b77e2079f27d3
Seth Stephens <sethastephens8@gmail.com>
2021-09-02 10:09:34 -0400

    none

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,15 @@ import image4 from "../images/carousel14.jpg";
 import Image1 from '../media/image1.jpg'
 import Image2 from '../media/image2.jpg'
 import Image3 from '../media/image3.jpg'
+import weather from '../api/weatherApi';
+
 
 const Home = () => {  
 
     const date = new Date();
   const day = date.getFullYear();
+  
+
 
   const articles = [
     {
@@ -44,7 +48,52 @@ const Home = () => {
       title: "Big Bad Ball",
       snippet:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor saepe quasi eaque tempora sint "
-    }
+    },
+    {
+        id: 4,
+        backgroundImg: Image1,
+        category: "Sports",
+        date: day,
+        title: "Big Bad Ball",
+        snippet:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor saepe quasi eaque tempora sint "
+      },
+      {
+        id: 5,
+        backgroundImg: Image2,
+        category: "Sports",
+        date: day,
+        title: "Big Bad Ball",
+        snippet:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor saepe quasi eaque tempora sint "
+      },
+      {
+        id: 6,
+        category: "Sports",
+        backgroundImg: Image3,
+        date: day,
+        title: "Big Bad Ball",
+        snippet:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor saepe quasi eaque tempora sint "
+      },
+      {
+        id: 7,
+        category: "Sports",
+        backgroundImg: Image3,
+        date: day,
+        title: "Big Bad Ball",
+        snippet:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor saepe quasi eaque tempora sint "
+      },
+      {
+        id: 8,
+        category: "Sports",
+        backgroundImg: Image3,
+        date: day,
+        title: "Big Bad Ball",
+        snippet:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dolor saepe quasi eaque tempora sint "
+      }
   ];
 
   return (
