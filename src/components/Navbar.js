095f1a930294987e52bd973d4282685a960f9535
Seth Stephens <sethastephens8@gmail.com>
2021-09-03 15:33:49 -0400

    saved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const Navbar = ( {handleLoginClick}) => {
-    const handleClick = () =>{
-     handleLoginClick();
-    }
+const Navbar = ({ handleLoginClick }) => {
+  const handleClick = () => {
+    handleLoginClick();
+  };
 
-    return (
-        <nav className="nav-wrapper black darken-3">
-            <div className="container">
-            <a href="_" className="brand-logo">Cincinnati Attractions</a>
-            <ul className="right">
-            <li> <a href="/"> Home </a></li>
-            <li> <a href="/Shop"> Shop </a></li>
-             <li> <a href="/Blog"> Blog </a></li>
-             <li> <a href="/About"> About </a></li>
-             <li><a onClick={handleClick} className="login"> sign up</a></li>
-            
-        </ul>
-            </div>
-        </nav>
+  return (
+    <nav className="nav-wrapper navbar-dark navbar-expand-lg black darken-3">
+      <div className="container-fluid">
+        <a href="_" className="brand-logo">
+          Cincinnati Attractions
+        </a>
+        <button
+          className="navbar-toggler "
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-    )
-}
-
-export default Navbar
+export default Navbar;
