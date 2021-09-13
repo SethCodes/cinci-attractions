38bda001df2069a589cd6483e9164b307b5427c0
Guillermo Cabral <guille.cabral87@gmail.com>
2021-09-13 15:00:28 -0400

    sigin form, home page blogs beautified

--- src/components/Home.js
+++ src/components/Home.js
@@ -156,7 +156,7 @@ console.log(attractions);
       </div>
 
      <div className="container-fluid">
-     <div className="row">
+     <div className="row" id="attractRow">
           {attractions.map((attraction) => {
             return (
               <div
@@ -165,6 +165,7 @@ console.log(attractions);
                 style={{
                   backgroundImage: `url("${attraction.imageUrl}")`
                 }}
+                id="attractImage"
               >
                 <AttractionCard
                   id={attraction.id}
