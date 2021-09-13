37c5cfd9d07ce9c8b8e96d0d5f16af855dc0ef68
Guillermo Cabral <guille.cabral87@gmail.com>
2021-09-13 13:09:30 -0400

    card news re-fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ContactForm from './components/ContactForm';
 import About from './components/About';
 import Event from './components/Event';
 import Footer from './components/Footer';
-import Blog from './components/Blog';
 import {BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
@@ -23,24 +22,6 @@ const App = () => {
 
     return(
       <BrowserRouter>
-<<<<<<< HEAD
-    <div className="App">
-      <Navbar handleLoginClick={handleLoginCLick}  />
-      <LoginForm isShowLogin={isShowLogin} />
-       
-      <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/about' component={About}/>
-      <Route path='/Event' component={Event}/>
-      <Route path='/Blog' component={Blog}/>
-      
-      </Switch>
-      <Footer />
-    </div>
-    
-
-    </BrowserRouter>
-=======
         <div className="App">
           <Navbar handleLoginClick={handleLoginCLick} handleContactClick={handleContactCLick} />
           <LoginForm isShowLogin={isShowLogin} />
@@ -53,7 +34,6 @@ const App = () => {
           <Footer />  
         </div>
       </BrowserRouter>
->>>>>>> d29df1f20b7f55873558f78b7b18de1674a6cf0c
     
     );
   }
