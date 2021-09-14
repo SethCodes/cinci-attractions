72c1d99218c91edf9f3dae2faa5f307cbcc9265e
Guillermo Cabral <guille.cabral87@gmail.com>
2021-09-14 13:11:46 -0400

    fixing news api

--- src/api/headlines.js
+++ src/api/headlines.js
@@ -10,10 +10,10 @@ class Headlines extends Component {
         };
     }
  
-    componentDidMount() {
+    componentDidMount = async () => {
         const apiUrl = process.env.HEADLINES_URI ||
             "https://newsapi.org/v2/everything?q=cincinnati&pageSize=8&apiKey=bbac26f2a1734825959bac16ed90df5c";
-        axios.get(apiUrl)
+        await axios.get(apiUrl)
             .then((response) => {
                 this.setState({
                     news: response.data.articles
